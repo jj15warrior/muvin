c9025e87bc6d23b76a59707a24c5029872cf2ff2
jj15 <janek302010@gmail.com>
2023-03-22 19:32:52 +0100

    more modular frame code

--- app/src/main/java/com/jj15/muffin/CacheNetController.java
+++ app/src/main/java/com/jj15/muffin/CacheNetController.java
@@ -1,36 +1,42 @@
 package com.jj15.muffin;
+import androidx.annotation.Nullable;
+
 import com.jj15.muffin.*;
 
 import java.util.ArrayList;
 
+//todo: remove nullable annotations -> testing only
+
 public class CacheNetController {
-    public PinFull getPinFull(String uuid) {
+    public PinFull getPinFull(String uuid,@Nullable boolean offlinemode) {
         //TODO: implement
         return null;
     }
-    public PinMinimal getPinMinimal(String uuid) {
+    public PinMinimal getPinMinimal(String uuid,@Nullable boolean offlinemode) {
         return null;
 
     }
-    public User getUser(String uuid) {
+    public User getUser(String uuid,@Nullable boolean testmode) {
         return null;
     }
-    public User me() {
+    public User me(@Nullable boolean testmode) {
         return null;
     }
-    public int addPin(PinMinimal pin) {
+    public int addPin(PinMinimal pin, @Nullable boolean offlinemode) {
         return 0; //status code
     }
-    public int addComment(PinComment comment, String pinUUID) {
+    public int addComment(PinComment comment, String pinUUID, @Nullable boolean offlinemode) {
         return 0; //status code
     }
-    public int addFavorite(String pinUUID) {
+    public int addFavorite(String pinUUID, @Nullable boolean offlinemode) {
         return 0; //status code
     }
-    public int removeFavorite(String pinUUID) {
+    public int removeFavorite(String pinUUID, @Nullable boolean offlinemode) {
         return 0; //status code
     }
     public boolean syncCache() {
+        //probably hardest part -> sync cache with server -> which cache should stay in case of conflict?
+        //TODO: implement
         return false; //should be true if successful
     }
     public ArrayList<PinMinimal> getNearbyPins(double lat, double lon, double radius) {
@@ -42,7 +48,10 @@ public class CacheNetController {
     public ArrayList<PinMinimal> getMyPins() {
         return null;
     }
-    public ArrayList<PinComment> getComments(String pinUUID) {
+    public ArrayList<PinComment> getComments(String pinUUID,@Nullable boolean testmode, @Nullable boolean offlinemode) {
+        return null;
+    }
+    public ArrayList<PinMinimal> search(String query) {
         return null;
     }
 
--- app/src/main/java/com/jj15/muffin/User.java
+++ app/src/main/java/com/jj15/muffin/User.java
@@ -2,6 +2,8 @@ package com.jj15.muffin;
 
 import android.media.Image;
 
+import androidx.annotation.Nullable;
+
 import java.util.ArrayList;
 
 public class User {
@@ -19,4 +21,27 @@ public class User {
         this.favorites = favorites;
         this.uuid = uuid;
     }
+    public boolean login(String uname, String password,@Nullable boolean testmode) {
+        if(testmode){
+            this.uname = "testuser";
+            this.email = "jj15@jj15.tech";
+            this.avatar = null;
+            this.pins = new ArrayList<PinMinimal>();
+            this.favorites = new ArrayList<PinMinimal>();
+            this.uuid = "user-uuid-test-123";
+            return true;
+        }
+        return false;
+    }
+    public boolean register(String uname, String password, String email) {
+        return false;
+    }
+    public boolean logout(@Nullable boolean testmode) {
+        if(testmode){
+            return true;
+        }
+        return false; //TODO: implement
+
+    }
+
 }
